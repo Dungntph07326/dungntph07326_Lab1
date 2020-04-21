0a6eaf18eece307a1f7e7ab53b365d8656ab3175
Dungntph07326 <62126484+Dungntph07326@users.noreply.github.com>
2020-04-21 20:56:19 +0700

    Update truutuong.php

--- truutuong.php
+++ truutuong.php
@@ -1,3 +1,36 @@
 <?php
-echo "Hello word";
+//vd1 : tính trừu tượng
+class personnel {
+	var $name;
+	var $avatar;
+	var $work;
+	var $phone;
+	var $address;
+
+	function goToWork(){
+
+	}
+	function takeTheJob(){
+
+	}
+}
+//vd 2
+ class customer {
+ 	var $name;
+ 	var $phone;
+ 	var $email;
+ 	var $address;
+
+
+ 	function order(){
+
+ 	}
+ 	function pay(){
+
+ 	}
+ 	function feedBack(){
+
+ 	}
+ }
+
 ?>
